c7866416c5ad26e8372a14e5ec8a2a4527bde900
yuanshikai524 <gcobc04992@gmail.com>
2022-12-01 23:54:24 +0800

    加上了style的屬性對象的型別

--- src/components/LeftColumn.tsx
+++ src/components/LeftColumn.tsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react"
 
+interface CssProp {
+  minHeight: string   
+}
+
 const LeftColumn = () => {
 
-  let [winWidth, setWinWidth] = useState<number>(window.innerWidth > 1400 ? 1400 : window.innerWidth)
+  const [winWidth, setWinWidth] = useState<number>(window.innerWidth > 1400 ? 1400 : window.innerWidth)
+
+  const styleProp: CssProp = {minHeight: winWidth < 713 ? `${winWidth * 0.75 - 8}px` : `${winWidth * 0.3333 * 0.75 - 8}px`};
 
   // 加上resize監聽事件，監聽innerWidth是否變動，有變動則改變state的值
   useEffect(() => {
-    window.addEventListener("resize", () => {
+    window.addEventListener("resize", (): void => {
       if(window.innerWidth > 1400) {
         setWinWidth(1400);
       } else {
@@ -17,7 +23,7 @@ const LeftColumn = () => {
 
   return (
     <div className='col-4 col left-col shadow'>
-      <div className="portrait-container" style={{ minHeight: winWidth < 713 ? `${winWidth * 0.75 - 8}px` : `${winWidth * 0.3333 * 0.75 - 8}px` }}>
+      <div className="portrait-container" style={styleProp}>
         <img src='/images/portrait.jpg' alt="portrait" />
         <span className="my-name">Jeff Chen</span>
       </div>
