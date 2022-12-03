46674e6a3f9e9dc8724ea1ebb50127f9a457a78c
yuanshikai524 <gcobc04992@gmail.com>
2022-12-04 02:11:01 +0800

    完成了左欄樣式與資料，接著做右欄

--- src/components/LeftColumn.tsx
+++ src/components/LeftColumn.tsx
@@ -8,12 +8,12 @@ const LeftColumn = () => {
 
   const [winWidth, setWinWidth] = useState<number>(window.innerWidth > 1400 ? 1400 : window.innerWidth)
 
-  const styleProp: CssProp = {minHeight: winWidth < 713 ? `${winWidth * 0.75 - 8}px` : `${winWidth * 0.3333 * 0.75 - 8}px`};
+  const styleProp: CssProp = { minHeight: winWidth < 713 ? `${winWidth * 0.75 - 8}px` : `${winWidth * 0.3333 * 0.75 - 8}px` };
 
   // 加上resize監聽事件，監聽innerWidth是否變動，有變動則改變state的值
   useEffect(() => {
     window.addEventListener("resize", (): void => {
-      if(window.innerWidth > 1400) {
+      if (window.innerWidth > 1400) {
         setWinWidth(1400);
       } else {
         setWinWidth(window.innerWidth);
@@ -58,8 +58,51 @@ const LeftColumn = () => {
             </li>
           </ul>
         </div>
-        <div>Skill</div>
-        <div>Language</div>
+        <div className="section">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-flag-fill" viewBox="0 0 16 16">
+              <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001" />
+            </svg>
+            <span className="title">Skills</span>
+          </div>
+          <span className="item-name">JavaScript</span>
+          <div className="outside">
+            <div className="inside inside-javascript">95%</div>
+          </div>
+          <span className="item-name">React</span>
+          <div className="outside">
+            <div className="inside inside-react">85%</div>
+          </div>
+          <span className="item-name">TypeScript</span>
+          <div className="outside">
+            <div className="inside inside-typescript">70%</div>
+          </div>
+          <span className="item-name">Sass</span>
+          <div className="outside">
+            <div className="inside inside-sass">70%</div>
+          </div>
+        </div>
+        <div className="section">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-globe-asia-australia" viewBox="0 0 16 16">
+              <path d="m10.495 6.92 1.278-.619a.483.483 0 0 0 .126-.782c-.252-.244-.682-.139-.932.107-.23.226-.513.373-.816.53l-.102.054c-.338.178-.264.626.1.736a.476.476 0 0 0 .346-.027ZM7.741 9.808V9.78a.413.413 0 1 1 .783.183l-.22.443a.602.602 0 0 1-.12.167l-.193.185a.36.36 0 1 1-.5-.516l.112-.108a.453.453 0 0 0 .138-.326ZM5.672 12.5l.482.233A.386.386 0 1 0 6.32 12h-.416a.702.702 0 0 1-.419-.139l-.277-.206a.302.302 0 1 0-.298.52l.761.325Z" />
+              <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM1.612 10.867l.756-1.288a1 1 0 0 1 1.545-.225l1.074 1.005a.986.986 0 0 0 1.36-.011l.038-.037a.882.882 0 0 0 .26-.755c-.075-.548.37-1.033.92-1.099.728-.086 1.587-.324 1.728-.957.086-.386-.114-.83-.361-1.2-.207-.312 0-.8.374-.8.123 0 .24-.055.318-.15l.393-.474c.196-.237.491-.368.797-.403.554-.064 1.407-.277 1.583-.973.098-.391-.192-.634-.484-.88-.254-.212-.51-.426-.515-.741a6.998 6.998 0 0 1 3.425 7.692 1.015 1.015 0 0 0-.087-.063l-.316-.204a1 1 0 0 0-.977-.06l-.169.082a1 1 0 0 1-.741.051l-1.021-.329A1 1 0 0 0 11.205 9h-.165a1 1 0 0 0-.945.674l-.172.499a1 1 0 0 1-.404.514l-.802.518a1 1 0 0 0-.458.84v.455a1 1 0 0 0 1 1h.257a1 1 0 0 1 .542.16l.762.49a.998.998 0 0 0 .283.126 7.001 7.001 0 0 1-9.49-3.409Z" />
+            </svg>
+            <span className="title">Languages</span>
+          </div>
+          <span className="item-name">Chinese</span>
+          <div className="outside">
+            <div className="language inside inside-chinese"></div>
+          </div>
+          <span className="item-name">Japanese</span>
+          <div className="outside">
+            <div className="language inside inside-japanese"></div>
+          </div>
+          <span className="item-name">English</span>
+          <div className="outside">
+            <div className="language inside inside-english"></div>
+          </div>
+        </div>
       </div>
     </div>
   )
