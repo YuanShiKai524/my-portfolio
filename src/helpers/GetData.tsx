0a643633dca4ebab87b44c765e11057a93be6630
yuanshikai524 <gcobc04992@gmail.com>
2022-12-07 05:39:41 +0800

    把工作經歷那一塊的遍歷成功了，但還得改成能夠展示不同內容

--- src/helpers/GetData.tsx
+++ src/helpers/GetData.tsx
@@ -8,10 +8,7 @@ interface Props {
 interface Title<S = string> {
   name: S
   svgClass: S
-  path: {
-    1: S
-    2: S
-  }
+  path: {[key: string]: S}
 }
 
 interface Work<S = string> {
@@ -20,11 +17,7 @@ interface Work<S = string> {
   year: S
   location: S
   industry: S
-  descriptions: {
-    1: S
-    2: S
-    3: S
-  }
+  descriptions: {[key: string]: S}
 }
 
 interface Education<S = string> {
@@ -34,14 +27,7 @@ interface Education<S = string> {
   status: S
 }
 
-interface Data {
-  title: Title
-  college?: Education
-  high_school?: Education
-  root2?: Work
-  hnw?: Work
-  kura?: Work
-}
+type Data = {[key: string]: Title | Education | Work}
 
 interface DataStatus {
   isLoading: boolean,
@@ -83,7 +69,7 @@ const GetData = ({dataUrl}: Props): JSX.Element => {
   }, [])
 
   const { isLoading, errorMsg, data } = dataStatus
-  const { name, svgClass, path } = data.title
+  const { name, svgClass, path } = data.title as Title
 
   return (
     <>
@@ -100,27 +86,36 @@ const GetData = ({dataUrl}: Props): JSX.Element => {
           </h1>
           <div className="content-section">
             {
-              
+              Object.keys(data).map((key) => {
+                if (key !== 'title') {
+                  const { company, position, year, location, industry, descriptions } = data[key] as Work
+                  return (
+                    <div className="content-container" key={key}>
+                      <h3>{company}</h3>
+                      <h3>{position}</h3>
+                      <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar" viewBox="0 0 16 16">
+                          <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                        </svg>
+                        {year}
+                      </span>
+                      <div className="sub-content">
+                        地點：{location}<br />
+                        產業：{industry}<br /><br />
+                        工作內容：<br />
+                        <ul>
+                          {
+                            Object.keys(descriptions).map((key) => {
+                              return <li key={key}>{descriptions[key]}</li>
+                            })
+                          }
+                        </ul>
+                      </div>
+                    </div>
+                  )
+                }
+              })
             }
-            <div className="content-container">
-              <h3>株式会社ROOT2</h3>
-              <h3>飯店櫃檯人員</h3>
-              <span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar" viewBox="0 0 16 16">
-                  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
-                </svg>
-                Apr&nbsp;2019&nbsp;-&nbsp;Nov&nbsp;2021
-              </span>
-              <div className="sub-content">
-                地點：日本京都。<br />產業：飯店經營業。<br /><br />
-                工作內容：<br />
-                <ul>
-                  <li>處理入住及退房、接洽預約、後台系統管理訂單及安排房間、處理及應對客人入住期間所遇到的問題。</li>
-                  <li>備品的庫存管理、與預約平台業者或飯店房間內之用品廠商的接洽、英日中三語翻譯、客戶資料建檔及管理。</li>
-                  <li>處理客訴、分配工讀生工作項目及監管其業務效率、培訓新人、協助打掃人員飯店內各處環境衛生處理、製作及販賣餐點及飲品等所有飯店之相關業務。</li>
-                </ul>
-              </div>
-            </div>
           </div>
         </>
       }
